a28b29c75912e134f006865630eb99ac942f0fff
陈缘科技 <chenkai051123@outlook.com>
2025-02-18 12:43:31 +0800

    2025.02.18 update

---
+++ 02/PHP/01/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="zh">
+	<head>
+		<meta charset="UTF-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<title>哪吒2</title>
+	</head>
+	<body>
+		<?php
+            echo "若前方无路，<br />";
+            echo "我便踏出一条路；<br />";
+            echo "若天理不容，<br />";
+            echo "我便扭转这乾坤。<br />";
+            echo "因为我还年轻，<br />";
+            echo "我想试试！";
+        ?>
+	</body>
+</html>
